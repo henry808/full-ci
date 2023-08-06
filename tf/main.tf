54d4e3ea4c22c7f0f9689e8fe5317375dbe14a45
henry808v2 <hgrantham@gmail.com>
2023-08-05 17:07:14 -0700

    Add .gitignore; Fix ALB SG, etc.

--- tf/main.tf
+++ tf/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 variable "env" {
-  default = "${env}"
+  default = "prod"
 }
 
 # Configure the AWS Provider
@@ -18,28 +18,28 @@ provider "aws" {
 }
 
 # AWS EC2
-resource "aws_instance" "webserver-ec2-${env}" {
+resource "aws_instance" "webserver-ec2-prod" {
   ami = "ami-03f65b8614a860c29" # ubuntu 22.04 x86
   instance_type = "t2.micro"
 
   # Reference the security group
-  vpc_security_group_ids = [aws_security_group.webserver-ec2-sg-${env}.id]
+  vpc_security_group_ids = [aws_security_group.webserver-ec2-sg-prod.id]
 
   tags = {
-    Name = "webserver-ec2-${env}"
+    Name = "webserver-ec2-${var.env}"
   }
 
-  key_name = aws_key_pair.webserver-ec2-key-pair-${env}.key_name
+  key_name = aws_key_pair.webserver-ec2-key-pair-prod.key_name
 }
 
-resource "aws_key_pair" "webserver-ec2-key-pair-${env}" { 
-  key_name = "webserver-ec2-key-pair-${env}"
+resource "aws_key_pair" "webserver-ec2-key-pair-prod" { 
+  key_name = "webserver-ec2-key-pair-${var.env}"
   public_key = file("~/.ssh/gitlab072723.pub")
 }
 
 # SG for EC2 instance
-resource "aws_security_group" "webserver-ec2-sg-${env}" {
-  name        = "webserver-ec2-sg-${env}"
+resource "aws_security_group" "webserver-ec2-sg-prod" {
+  name        = "webserver-ec2-sg-${var.env}"
   
   # SSH access
   ingress {
@@ -58,12 +58,13 @@ resource "aws_security_group" "webserver-ec2-sg-${env}" {
     description = "Allow HTTP traffic on port 8080"
   }
 
-  ingress {
-  from_port                = 8080 
-  to_port                  = 8080
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.webserver_alb_sg-${env}.id
-}
+  # # ALB http access
+  # ingress {
+  # from_port                = 8080 
+  # to_port                  = 8080
+  # protocol                 = "tcp"
+  # source_security_group_id = aws_security_group.webserver_alb_sg-prod.id
+  # }
   
   # download or install from anywhere
   egress {
@@ -75,53 +76,53 @@ resource "aws_security_group" "webserver-ec2-sg-${env}" {
 
   # name
   tags = {
-    Name = "webserver-ec2-sg-${env}"
+    Name = "webserver-ec2-sg-${var.env}"
   }
 }
 
 # Define ALB Listener and Target group and SG
 
 # SG for ALB
-resource "aws_security_group" "webserver_alb_sg-${env}" {
-  name        = "webserver_alb_sg-${env}"
+resource "aws_security_group" "webserver_alb_sg-prod" {
+  name        = "webserver_alb_sg-${var.env}"
   
-  # Allow inbound HTTP traffic on port 80 from anywhere
+  # Allow inbound HTTP traffic on port 8080 from anywhere (listener)
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    source_security_group_id = aws_security_group.webserver-ec2-${env}.id
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 
   # Allow all outbound traffic
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    source_security_group_id = aws_security_group.webserver-ec2-prod.id
   }
 
   tags = {
-    Name = "webserver_alb_sg-${env}"
+    Name = "webserver_alb_sg-${var.env}"
   }
 }
 
 # ALB
-resource "aws_lb" "webserver-alb-${env}" {
-  name               = "webserver-alb-${env}"
+resource "aws_lb" "webserver-alb-prod" {
+  name               = "webserver-alb-${var.env}"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.webserver_alb_sg-${env}.id]
+  security_groups    = [aws_security_group.webserver_alb_sg-prod.id]
   subnets            = ["subnet-ec528b94", "subnet-6619b13b", "subnet-3df00a77"] # using default subnets a,b,c
 
   tags = {
-    Name = "webserver-alb-${env}"
+    Name = "webserver-alb-${var.env}"
   }
 }
 
 # Target Group
-resource "aws_lb_target_group" "webserver-tg-${env}" {
-  name     = "webserver-tg-${env}"
+resource "aws_lb_target_group" "webserver-tg-prod" {
+  name     = "webserver-tg-${var.env}"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = "vpc-38dd9140"
@@ -138,19 +139,19 @@ resource "aws_lb_target_group" "webserver-tg-${env}" {
   }
 
   tags = {
-    Name = "webserver-tg-${env}"
+    Name = "webserver-tg-${var.env}"
   }
 }
 
 # ALB Listener
-resource "aws_lb_listener" "webserver-front-end-${env}" {
-  load_balancer_arn = aws_lb.webserver-alb-${env}.arn
+resource "aws_lb_listener" "webserver-front-end-prod" {
+  load_balancer_arn = aws_lb.webserver-alb-prod.arn
   port              = 8080
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.webserver-tg-${env}.arn
+    target_group_arn = aws_lb_target_group.webserver-tg-prod.arn
   }
 
 }
